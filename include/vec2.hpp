90923b40061faf5e1f92eaa4a1d1c944c5a52759
MidnightMagenta <163724183+MidnightMagenta@users.noreply.github.com>
2024-04-22 09:19:01 +0200

    Window start

---
+++ include/vec2.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace lm
+{
+	struct vec2
+	{
+		double x = 0;
+		double y = 0;
+	};
+	struct vec2f
+	{
+		float x = 0;
+		float y = 0;
+	};
+	struct vec2i
+	{
+		int x = 0;
+		int y = 0;
+	};
+	struct vec2u
+	{
+		unsigned int x = 0;
+		unsigned int y = 0;
+	};
+}
--- include/window.hpp
+++ include/window.hpp
@@ -3,3 +3,25 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <string>
+#include "../include/vec2.hpp"
+
+namespace lm
+{
+	class Window
+	{
+	public:
+		Window() : window(NULL) {};
+		Window(lm::vec2u windowSize, std::string windowName) { CreateWindow(windowSize, windowName); }
+
+		void CreateWindow(lm::vec2u windowSize, std::string windowName);
+
+		lm::vec2u GetSize();
+		void SetSize(lm::vec2u);
+
+		lm::vec2u GetPosition();
+		void SetPosition(lm::vec2u);
+	private:
+		GLFWwindow* window;
+	};
+}
